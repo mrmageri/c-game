2f5d1a30cefe47937dedb01dc1b0e63bc2ac7407
Mikhail Markov <mikhaiil@mmarkovv.ru>
2023-11-20 17:25:22 +0300

    Fixed photo path depending on platform

--- src/Player.h
+++ src/Player.h
@@ -5,23 +5,33 @@
 #include <iostream>
 #include"MoveableRectangle.h"
 
+#if defined(_WIN64)
+const std::string PLAYER_FRONT = "../../../../resources/player/playerFront.png";
+const std::string PLAYER_BACK = "../../../../resources/player/playerBack.png";
+const std::string PLAYER_RIGHT = "../../../../resources/player/playerRight.png";
+const std::string PLAYER_LEFT = "../../../../resources/player/playerLeft.png";
+#else
 const std::string PLAYER_FRONT = "../../resources/player/playerFront.png";
 const std::string PLAYER_BACK = "../../resources/player/playerBack.png";
 const std::string PLAYER_RIGHT = "../../resources/player/playerRight.png";
 const std::string PLAYER_LEFT = "../../resources/player/playerLeft.png";
+#endif
 
 
 class Player : public MoveableRectangle {
 public:
-	Player(sf::RenderWindow* window);
-	Player(float x, float y, float w, float h, sf::RenderWindow* window, sf::Color color = sf::Color::White);
+    Player(sf::RenderWindow *window);
 
-	virtual ~Player() = default;
+    Player(float x, float y, float w, float h, sf::RenderWindow *window, sf::Color color = sf::Color::White);
 
-	virtual void ProcessEvent(sf::Event& event) final;
-	virtual void ProcessLogic(sf::RectangleShape& rectangleshape) final;
+    virtual ~Player() = default;
+
+    virtual void ProcessEvent(sf::Event &event) final;
+
+    virtual void ProcessLogic(sf::RectangleShape &rectangleshape) final;
 
 protected:
-	virtual void ProcessMovement(sf::RectangleShape& rectangleshape) final;
+    virtual void ProcessMovement(sf::RectangleShape &rectangleshape) final;
 };
-#endif // !BouncCircle 
+
+#endif // !BouncCircle
